8ce182ffd3e5c5950d1022a70fc50981b9e94af3
Keeylay <kyrutkowski@gmail.com>
2021-07-06 16:02:30 -0500

    JS Fun

---
+++ Javascript/predictdestructuring.js
@@ -0,0 +1,54 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar) // prints Tesla
+console.log(otherRandomCar) // prints mercedes
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); // prints undefined
+console.log(otherName); // prints Elon
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password); // prints 12345
+console.log(hashedPassword); // prints undefined
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second); // prints false 2 /= 5
+console.log(first == third); // prints true 2 = 2
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key); // print value
+console.log(secondKey); // 1,5,1,8,3,3
+console.log(secondKey[0]); // prints 1
+console.log(willThisWork); // prints 5
+
+
+
+
+
