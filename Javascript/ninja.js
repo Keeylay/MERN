8f272abaab6355b34466886b426ea80586eb810a
Keeylay <kyrutkowski@gmail.com>
2021-07-07 13:09:39 -0500

    Practice

---
+++ Javascript/ninja.js
@@ -0,0 +1,27 @@
+// parent class ninja
+class Ninja {
+    constructor(ninjaname, health, speed, strength) {
+        this.ninjaname = ninjaname;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    codename() {
+        console.log(`Hello my name is ${ this.ninjaname }`)
+    }
+
+    showstats() {
+        console.log(`Name: ${ this.ninjaname }, Strength ${ this.stength }, Speed: ${ this.speed }, Health: ${ this.health }`)
+    }
+
+    drinksake () {
+        this.health += 10;
+        console.log(`Health is now ${ this.health }`)
+    }
+}
+
+const newninja = new Ninja("naruto", 100, 3, 5);
+    newninja.codename();
+    newninja.showstats();
+    newninja.drinksake();
